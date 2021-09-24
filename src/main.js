c573aef33eccb3aad4cd402ef2497b64dd8f97e0
fujia_zhang <i@iamzfj.cn>
2021-09-24 21:44:47 +0800

    fe：移除额外插件

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import lazyPlugin from 'vue3-lazy';
 import loadingDirective from '@/components-base/loading/directive'
-import Notifications from 'vue3-vt-notifications'
 
 import '@/common/stylus/index.styl'
 
@@ -12,7 +11,6 @@ const app = createApp(App)
 
 app.use(store)
 app.use(router)
-app.use(Notifications)
 app.use(lazyPlugin, {
   loading: require('@/common/image/default.png'),
   error: ''
